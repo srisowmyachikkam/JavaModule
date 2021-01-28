bb73baf73caa0cc706e2f0b7fac7d61fa27abe00
srisowmyachikkam <75778093+srisowmyachikkam@users.noreply.github.com>
2021-01-28 22:02:02 +0530

    Main.java

---
+++ Java Assignment #8/Main.java
@@ -0,0 +1,12 @@
+package oops_module;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ErrorHandling object = new ErrorHandling();
+		object.test();
+
+	}
+
+}
