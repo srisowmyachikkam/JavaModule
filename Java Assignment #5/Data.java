ab2487540597eb7ed3f1ea5f7a456d10320d5178
srisowmyachikkam <75778093+srisowmyachikkam@users.noreply.github.com>
2021-01-28 19:00:56 +0530

    Data.java

---
+++ Java Assignment #5/Data.java
@@ -0,0 +1,25 @@
+package sowmya.assignment5.data;
+
+public class Data {
+	int variable1;
+	char variable2;
+	public void display1()
+	{
+		System.out.println(variable1);
+		System.out.println(variable2);
+	}
+	
+	public void display2( )
+	{
+		int localVariable1;
+		char localVariable2;
+		
+	  /*System.out.println(localVariable1);
+		System.out.println(localVariable2);
+		
+		The local variable values cannot be printed unlike the member variables 
+		The member variables take the default values by the compiler whereas local variables throws an error
+		*/
+	}
+
+}
